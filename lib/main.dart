57b62b58089711d3f024663e5e1ca7ef5ad68b07
sn-shakib <snrsstt6261@gmail.com>
2024-03-04 12:43:26 +0600

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return  Placeholder();
+  }
+}
